52054bceb4a9bf59b090bc8dfe2784baefa7f102
Danil Nazarko <danilspy36@gmail.com>
2024-05-31 12:38:12 +0300

    docker
    add DockerFile

--- obj/Debug/net8.0/ttk_bot.AssemblyInfo.cs
+++ obj/Debug/net8.0/ttk_bot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ttk_bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be82f8c67103f317dc392db54e584e4bb07c40e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f5175e8ed3cfccbd1f6eee07bb44d57e03f6ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("ttk_bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ttk_bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
